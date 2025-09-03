21c990ca84128357886a11f4a11924463a1d6b0b
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-09-03 15:24:31 +0100

    Add user login handler to Users controller
    Introduces the handleLogin method for authenticating users via email or username. Returns a JWT token upon successful login and handles error cases for invalid credentials and token creation.

--- backend/src/controllers/Users.js
+++ backend/src/controllers/Users.js
@@ -38,3 +38,60 @@ const handleRegister = async (req, res) => {
     res.json({ message: "An error occurred while registering the user." });
   }
 };
+
+// login method
+const handleLogin = async (req, res) => {
+  let data = "";
+  let {
+    email = req.body.email,
+    username = req.body.username,
+    password = req.body.password,
+  } = data;
+
+  const isEmail = (email) => {
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  };
+
+  if (!isEmail(email)) {
+    username = req.body.email;
+  }
+
+  try {
+    const user = await User.findOne(username ? { username } : { email });
+    if (!user) {
+      return res.status(404).json({ message: "Credentials are invalid" });
+    } else {
+      await user.comparePasswords(password).then((result) => {
+        if (result) {
+          const id = user._id;
+          const username = user.username;
+          const role = user.role;
+          const myToken = jwt.sign(
+            { id, username, role },
+            process.env.JWT_ACCESS_SECRET
+          );
+          if (myToken) {
+            console.log({ myToken });
+            return res.json({ myToken });
+          } else {
+            console.log("Token was not created");
+            return res.json({ message: "Token was not created" });
+          }
+        } else {
+          console.log("Password is incorrect");
+          res.status(404).json({ message: "Password is incorrect" });
+        }
+      });
+    }
+  } catch (error) {
+    console.log("An error occurred");
+    return res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+module.exports = {
+  handleRegister,
+  handleLogin,
+};
