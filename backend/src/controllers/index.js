fe3277b6fc978c1ec1145e8ba5b464dac419ac12
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-09-03 15:35:58 +0100

    Add image upload functionality
    Introduced multer-based image upload with file type filtering and disk storage configuration for profile images. Exported uploadImage and Users for use in other modules.

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,49 @@
+const Users = require("./Users");
+const User = require("../models/Users");
+
+// multer
+const path = require("path");
+const multer = requier("multer");
+
+// storage
+const storage = (pathName) => {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, pathName);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+};
+
+const fFilter = (req, file, cb) => {
+  // allowed ext
+  const filetypes = /jpeg|jpg|png/;
+
+  // check ext
+  const extname = filetypes.test(
+    path.extname(file.originalname).toLocaleLowerCase()
+  );
+
+  //   check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Image only");
+  }
+};
+
+const uploadImage = multer({
+  fileFilter: fFilter,
+  storage: storage(
+    path.join(path.dirname(__dirname), "public", "images", "profile")
+  ),
+});
+
+module.exports = {
+  uploadImage,
+  Users,
+};
